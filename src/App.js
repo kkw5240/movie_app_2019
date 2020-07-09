7bee4539b7979a68a1971fd487c53753caa6c5cf
kkw5240 <kkw5240@gmail.com>
2020-07-10 00:22:34 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const foodILike = [
   {
@@ -11,40 +12,53 @@ const foodILike = [
     id: 1,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      rating: 4.5
   },
   {
     id: 2,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      rating: 4
   },
   {
     id: 3,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+      rating: 4.3
   },
   {
     id: 4,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+      rating: 3.5
   }
 ];
 
 const renderFood = (dish) => {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+  return <Food key={dish.id} 
+    name={dish.name} 
+    picture={dish.image}
+    rating={dish.rating} />;
 };
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
+      <h4>{rating} / 5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 function App() {
   return (
